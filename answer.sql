5467c7e7bf5f6bfc56e2aaf8bca2d340d97cc066
Garret Larson <garret.hs.larson@gmail.com>
2020-10-13 12:09:23 -0500

    another question done.

--- answer.sql
+++ answer.sql
@@ -1,7 +1,9 @@
 -- Get all customers and their addresses....CHECK
+-- check what's in these tables
 -- select * from customers;
 -- select * from addresses;
 
+-- my answer:
 -- select c.first_name, c.last_name, a.street, a.city, a.state, a.zip from customers c join addresses a on c.id=a.customer_id;
 
 -- Get all orders and their line items (orders, quantity and product).
@@ -9,7 +11,7 @@
 -- select * from orders; --order here
 -- select * from line_items; --quantity here
 -- select * from products; -- product here
--- --answer:
+-- my answer:
 -- select o.id as orderId, li.quantity as productQuantity, p.description as productInOrder from orders as o join line_items as li on o.id=li.order_id join products as p on li.product_id=p.id;
 
 -- Which warehouses have cheetos?
@@ -17,7 +19,7 @@
 -- select * from warehouse;
 -- select * from warehouse_product;
 
---answer:
+-- my answer:
 -- select wh.warehouse from warehouse as wh 
 -- join warehouse_product as whp on wh.id=whp.warehouse_id 
 -- join products as p on whp.product_id=p.id
@@ -25,6 +27,12 @@
 
 
 -- Which warehouses have diet pepsi?
+-- my answer:
+-- select wh.warehouse from warehouse as wh 
+-- join warehouse_product as whp on wh.id=whp.warehouse_id 
+-- join products as p on whp.product_id=p.id
+-- where p.description='diet pepsi';
+
 
 -- Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
 
