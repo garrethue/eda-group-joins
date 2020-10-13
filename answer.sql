766bd83d4737c5f79ac87abfc566fb3fbad4e44f
Garret Larson <garret.hs.larson@gmail.com>
2020-10-13 13:56:50 -0500

    Finished stretch.

--- answer.sql
+++ answer.sql
@@ -1,6 +1,8 @@
 -- 1. Get all customers and their addresses
 -- my answer:
--- select c.first_name, c.last_name, a.street, a.city, a.state, a.zip from customers c join addresses a on c.id=a.customer_id;
+-- select c.first_name, c.last_name, a.street, a.city, a.state, a.zip 
+-- from customers c 
+-- join addresses a on c.id=a.customer_id;
 
 -- 2. Get all orders and their line items (orders, quantity and product).
 -- my answer:
@@ -47,3 +49,37 @@
 -- join warehouse_product as whp on p.id=whp.product_id
 -- where p.description='diet pepsi'
 -- group by p.description;
+
+--STRETCH below:
+
+-- 9. How much was the total cost for each order?
+-- my answer:
+-- select o.id as orderId, SUM((li.quantity*p.unit_price)) as totalcost
+-- from orders as o 
+-- join line_items as li on o.id=li.order_id 
+-- join products as p on li.product_id=p.id
+-- group by o.id
+-- order by o.id;
+
+-- 10. How much has each customer spent in total?
+-- my answer:
+-- select c.id as customerid, c.first_name, c.last_name, SUM(li.quantity*p.unit_price) as totalspent
+-- from customers c 
+-- join addresses a on c.id=a.customer_id
+-- join orders o on a.id = o.address_id
+-- join line_items li on o.id=li.order_id
+-- join products p on li.product_id=p.id
+-- group by c.id
+-- order by c.id;
+
+
+-- 11. How much has each customer spent in total? Customers who have spent $0 should still show up in the table. It should say 0, not NULL (research coalesce).
+-- my answer:
+-- select c.id, c.first_name, c.last_name, SUM(coalesce(li.quantity,0)*coalesce(p.unit_price,0))
+-- from customers c 
+-- full join addresses a on c.id=a.customer_id
+-- full join orders o on a.id = o.address_id
+-- full join line_items li on o.id=li.order_id
+-- full join products p on li.product_id=p.id
+-- group by c.id
+-- order by c.id;
